02532693e256a32d091b00175664fb70fc3948a0
Pha NGUYEN <nganhpha.hcmus@gmail.com>
2020-07-08 16:45:13 +0700

    add lk loss constraint

--- lib/procedure/lk_loss.py
+++ lib/procedure/lk_loss.py
@@ -41,7 +41,6 @@ def lk_target_loss(batch_locs, batch_next, batch_fbak, batch_back, lk_config, vi
         _locations = batch_locs[ibatch, batch_locs.size(1)//2, ipts, :].cpu()
         _points = points[ibatch, ipts, :-1].cpu()
         dist = torch.dist(_locations, _points).cpu().detach().numpy().astype(np.int)
-        print(dist)
         if dist > lk_config.window:
           sequence_checks[ibatch, ipts] = False
 
